dd39ce2f1ea611d7b7ad03e0faad303f73ffd772
Sergio Morchón Poveda <sergio.morchon@outlook.com>
2023-10-22 20:25:02 +0200

    chore: Improve types and prettier

--- src/serialize.ts
+++ src/serialize.ts
@@ -1,94 +1,105 @@
-type Header<Item> = [fieldProperty: keyof Item, headerText: string];
-type Headers<Item> = ReadonlyArray<Header<Item>>;
+type Headers<Item> = ReadonlyArray<
+  [fieldProperty: keyof Item, headerText: string]
+>;
 type FieldSerializer<Item> = (
-	value: Item[typeof propertyName],
-	propertyName: keyof Item,
+  value: Item[typeof propertyName],
+  propertyName: keyof Item,
 ) => string;
 
 type SerializeOptions<Item> = Readonly<{
-	delimiter: string;
-	lineBreak: string;
-	serializeField: FieldSerializer<Item>;
+  delimiter: string;
+  lineBreak: string;
+  serializeField: FieldSerializer<Item>;
 }>;
 
-const escapeField = (field: string, options: SerializeOptions<any>) =>
-	/"|\r|\n/.test(field) || field.includes(options.delimiter)
-		? `"${field.replace(/"/g, '""')}"`
-		: field;
+const DELIMITER = "delimiter";
+const LINE_BREAK = "lineBreak";
+const SERIALIZE_FIELD = "serializeField";
 
-const createRecord = (
-	fields: ReadonlyArray<string>,
-	options: SerializeOptions<any>
+const escapeField = <Item>(field: string, options: SerializeOptions<Item>) =>
+  /"|\r|\n/.test(field) || field.includes(options[DELIMITER])
+    ? `"${field.replace(/"/g, '""')}"`
+    : field;
+
+const createRecord = <Item>(
+  fields: ReadonlyArray<string>,
+  options: SerializeOptions<Item>,
 ) =>
-	fields.map((field) => escapeField(field, options)).join(options.delimiter) +
-	options.lineBreak;
+  fields.map((field) => escapeField(field, options)).join(options[DELIMITER]) +
+  options[LINE_BREAK];
 
-const toString = (value: any) => {
-	if (value === undefined || value === null) {
-		return "";
-	}
+const toString = (value: unknown) => {
+  if (value === undefined || value === null) {
+    return "";
+  }
 
-	if (value instanceof Date) {
-		return value.toISOString();
-	}
+  if (value instanceof Date) {
+    return value.toISOString();
+  }
 
-	return String(value);
+  return String(value);
 };
 
-const defaultOptions: SerializeOptions<any> = {
-	delimiter: ";",
-	lineBreak: "\r\n",
-	serializeField: toString,
-};
-
-const createHeader = (headers: Headers<any>, options: SerializeOptions<any>) =>
-	createRecord(
-		headers.map(([, headerText]) => headerText),
-		options
-	);
-
-const createContent = (
-	headers: Headers<any>,
-	item: any,
-	options: SerializeOptions<any>
+const createHeader = <Item>(
+  headers: Headers<Item>,
+  options: SerializeOptions<Item>,
 ) =>
-	createRecord(
-		headers.map(([fieldProperty], headerIndex) =>
-			options.serializeField(item[fieldProperty], headers[headerIndex][0])
-		),
-		options
-	);
+  createRecord(
+    headers.map(([, headerText]) => headerText),
+    options,
+  );
+
+const createContent = <Item>(
+  headers: Headers<Item>,
+  item: Item,
+  options: SerializeOptions<Item>,
+) =>
+  createRecord(
+    headers.map(([fieldProperty], headerIndex) =>
+      options[SERIALIZE_FIELD](item[fieldProperty], headers[headerIndex][0]),
+    ),
+    options,
+  );
 
 type Signature<Item, Items> = [
-	items: Items,
-	headers: Headers<Item>,
-	options?: Partial<SerializeOptions<Item>>,
+  items: Items,
+  headers: Headers<Item>,
+  options?: Partial<SerializeOptions<Item>>,
 ];
 
 const normalizeArgs = <Item, Items>(...args: Signature<Item, Items>) => {
-	const [items, headers, options] = args;
-	return [items, headers, { ...defaultOptions, ...options }] as const;
+  const [items, headers, options] = args;
+  return [
+    items,
+    headers,
+    {
+      [DELIMITER]: ";",
+      [LINE_BREAK]: "\r\n",
+      [SERIALIZE_FIELD]: toString,
+      ...options,
+    },
+  ] as const;
 };
 
 export const serializeAsyncGenerator = async function* <Item>(
-	...args: Signature<Item, AsyncIterable<Item>>
+  ...args: Signature<Item, AsyncIterable<Item>>
 ) {
-	const [items, headers, options] = normalizeArgs(...args);
-	yield createHeader(headers, options);
-	for await (const item of items) {
-		yield createContent(headers, item, options);
-	}
+  const [items, headers, options] = normalizeArgs(...args);
+  yield createHeader(headers, options);
+  for await (const item of items) {
+    yield createContent(headers, item, options);
+  }
 };
 
 export const serializeGenerator = function* <Item>(
-	...args: Signature<Item, Iterable<Item>>
+  ...args: Signature<Item, Iterable<Item>>
 ) {
-	const [items, headers, options] = normalizeArgs(...args);
-	yield createHeader(headers, options);
-	for (const item of items) {
-		yield createContent(headers, item, options);
-	}
+  const [items, headers, options] = normalizeArgs(...args);
+  yield createHeader(headers, options);
+  for (const item of items) {
+    yield createContent(headers, item, options);
+  }
 };
 
 export const serialize = <Item>(...args: Signature<Item, Iterable<Item>>) =>
-	Array.from(serializeGenerator(...args)).join("");
+  Array.from(serializeGenerator(...args)).join("");
--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,15 @@ import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
 
 export default defineConfig({
-	plugins: [dts()],
-	build: {
-		lib: {
-			entry: "src/index.ts",
-			formats: ["es"],
-			fileName: "index",
-		},
-		outDir: "dist",
-		sourcemap: true,
-		emptyOutDir: true,
-	},
+  plugins: [dts()],
+  build: {
+    lib: {
+      entry: "src/index.ts",
+      formats: ["es"],
+      fileName: "index",
+    },
+    outDir: "dist",
+    sourcemap: true,
+    emptyOutDir: true,
+  },
 });
